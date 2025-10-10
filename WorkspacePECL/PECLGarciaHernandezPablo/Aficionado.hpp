f1e9cc00d2651cd5eaabc91cd1cbc96ea0f70e80
Blockky <blockygh6@hotmail.com>
2025-10-10 13:24:56 +0200

    Clases creadas

---
+++ WorkspacePECL/PECLGarciaHernandezPablo/Aficionado.hpp
@@ -0,0 +1,18 @@
+#ifndef AFICIONADO_HPP
+#define AFICIONADO_HPP
+
+class Aficionado
+{
+private:
+    int ID;
+    bool socio; // 1: Socio, 0: Simpatizante
+    int llegada;
+    
+public:
+    Aficionado();
+    
+    ~Aficionado();
+
+};
+
+#endif // AFICIONADO_HPP
