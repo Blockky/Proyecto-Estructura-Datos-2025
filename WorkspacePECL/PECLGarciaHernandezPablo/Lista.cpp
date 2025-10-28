1f1c700151cec8710281f840845cbd447449f73d
Sanzz850 <adrian.sanchezmartin@edu.uah.es>
2025-10-28 17:50:11 +0100

    Listas

--- WorkspacePECL/PECLGarciaHernandezPablo/Lista.cpp
+++ WorkspacePECL/PECLGarciaHernandezPablo/Lista.cpp
@@ -2,47 +2,44 @@
 
 Lista::Lista()
 {
-	primero = NULL;
-	ultimo = NULL;
+    primero = NULL;
+    ultimo = NULL;
 }
-void Lista::insertar_orden(Aficionado* a){
-	int llegada = a->getLlegada();
-	pnodoLista ptr;
-	
-	ptr = new NodoLista(a);
-	if(!primero){
-		primero = ptr;
-		ultimo = ptr;
-	} else {
-		if (llegada < primero->aficionado->getLlegada()){
-			ptr->siguiente = primero;
-			primero = ptr;
-		} else {
-			pnodoLista aux = primero;
-			while (aux->siguiente && 
-				   aux->siguiente->aficionado->getLlegada() < llegada){
-				 aux = aux->siguiente;
-		}
-		 ptr->siguiente = aux->siguiente;
-		aux->siguiente = ptr;
-		}
-		
-		if (ptr->siguiente == NULL) ultimo = ptr;
-		
-	}
+void Lista::insertar_orden(Aficionado* a)
+{
+    int llegada = a->getLlegada();
+    pnodoLista ptr;
+
+    ptr = new NodoLista(a);
+    if(!primero) {
+        primero = ptr;
+        ultimo = ptr;
+    } else {
+        if(llegada < primero->aficionado->getLlegada()) {
+            ptr->siguiente = primero;
+            primero = ptr;
+        } else {
+            pnodoLista aux = primero;
+            while(aux->siguiente && aux->siguiente->aficionado->getLlegada() < llegada) {
+                aux = aux->siguiente;
+            }
+            ptr->siguiente = aux->siguiente;
+            aux->siguiente = ptr;
+        }
+
+        if(ptr->siguiente == NULL)
+            ultimo = ptr;
+    }
 }
 void Lista::mostrar()
 {
-	pnodoLista aux = primero;
-	cout <<"\tLos aficionados en la lista son: ";
-	while(aux){
-		cout <<"->";
-		aux->aficionado->mostrar();
-		aux = aux->siguiente;
-	}
-	cout<<endl;
+    pnodoLista aux = primero;
+    cout << "\tLos aficionados en la lista son: ";
+    while(aux) {
+        cout << "->";
+        aux->aficionado->mostrar();
+        aux = aux->siguiente;
+    }
+    cout << endl;
 }
-Lista::~Lista()
-{
-}
-
+Lista::~Lista() {}
